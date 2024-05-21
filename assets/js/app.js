4d6091e08d15ca0b1df52ccfb11d5fb459d98a9b
Patricia-Cuevas <cuevasdpatricia@gmial.com>
2024-05-21 17:30:54 -0600

    correccion de codigo

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,23 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+const $n = document.querySelector('.name'); // cambiar a clases porque no estaba mandandolo a llamar como clase
+const $b = document.querySelector('.blog'); // cambiar a clases porque no estaba mandandolo a llamar como clase
+//const $l = document.querySelector('.location'); //eliminarlo o ponerlo en el html porque no está  
 
-function displayUser(username) {
+async function displayUser(username) { // se agrega a la funcion async para que podamos usar await
   $n.textContent = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`);
   console.log(data);
+  console.log(response); //se agrega el console 
   $n.textContent = '${data.name}';
   $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  //$l.textContent = '${data.location}'; // se comento porque no se usaba
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  $n.textContent = `Algo salió mal: ${err}` // se pone el simbolo de $
 }
 
 displayUser('stolinski').catch(handleError);
